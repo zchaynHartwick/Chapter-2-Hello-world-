57fbfeaceea7f75059f116df403950126f124cff
Zachary C. C <haynz@hartwick.edu>
2020-09-03 12:53:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# Start typing your program below this line...
+# This program says hello and asks for my name.
+print('Hello World!')
+print('What is your name?')
+myName = input()
+print('It is good to meet you, ' + myName)
+print('My name is Thalia')
+print('I am a computer that has been programmed to learn more about you, ' + myName)
+print('What is your full name? It must be more than just ' + myName)
+myNewName = input()
+print(myNewName + '... What a nice name')
+print('Where are you from?')
+myHome = input()
+print(myHome + ' you say? I can not say I have ever been there myself')
+print('What about your height... How tall are you?')
+myHeight = input()
+print('You see, I have no perception of height so I do not know wether or not ' + myHeight + ' is tall or short.')
+print('So let me get this right...')
+print('You are ' + myName + ' but your full name is ' + myNewName + '. You are from ' + myHome + ' and you are ' + myHeight)
+
+
+# when working with functions, you provide input.
+# The input is called _arguments_.
+# invoke
+# pass an _arguments_
+# return
